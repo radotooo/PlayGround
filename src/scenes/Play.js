788baf053e325dd07e11e8846893a26c349c9cfa
radotooo <radotooo@gmail.com>
2021-06-03 22:41:15 +0300

    wheel

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -24,6 +24,54 @@ export default class Play extends Scene {
     //   explosion.animate();
     // });
   }
+  circle() {
+    (($) => {
+      
+      const wheel = $('#wheel');
+      const arrow = $('#arrow');
+      const info = $('#info');
+    
+      const numItems = 8;
+      const arc = 360 / numItems;
+    
+      TweenLite.set(wheel, { rotation: 360 });
+    
+      const prizes = [
+        'Shirt',
+        'Free Bucket',
+        'Night Out #1',
+        'Je Vangt Bot',
+        'BioScoop Tickets',
+        'Night Out #2',
+        'Free Spin',
+        'HMH Tickets'
+      ];
+    
+      arrow.on('click', () => {
+      
+        // var random = Math.floor(Math.random() * 8000) + 6000;
+        const random = Math.floor(Math.random() * 800) + 600;
+        const tl = new TimelineMax();
+        tl 
+          .to(wheel, 7, { rotation: `+=${random}`, ease: Back.easeOut.config(1) });
+ 
+        TweenLite.to(tl, 4, {timeScale: 0, ease: Power1.easeOut, delay: 3 });
+      });
+    
+      TweenLite.ticker.addEventListener('tick', update);
+    
+      function update() {
+      
+        const rotation = wheel[0]._gsTransform.rotation;     
+        const angle = rotation - 360 * Math.floor(rotation / 360);     
+        const index = Math.floor((360 - angle) / arc) % prizes.length;    
+        const prize = prizes[index];
+      
+        info.html(`Current Prize = ${prize}`);
+      }
+       
+    })(jQuery);
+  }
 
   createElement() {
     const square = new Graphics();
